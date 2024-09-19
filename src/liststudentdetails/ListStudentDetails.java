3163bc9df1fb2703229b36b5d6155de35329725e
ShreyGondhiya <gondhiya@sheridancollege.ca>
2024-09-19 18:03:38 -0400

    studentlist version 3

--- src/liststudentdetails/ListStudentDetails.java
+++ src/liststudentdetails/ListStudentDetails.java
@@ -4,7 +4,7 @@
  */
 package liststudentdetails;
 
-/**
+/** Wednesday
  *
  * @author User
  */
@@ -30,10 +30,10 @@ public class ListStudentDetails {
         s3.setSid(3);
         
         //s1, s2, s3 - 3 students
-        Student[] list = new Student[3];
+        Student[] list = new Student[2];
         list[0] = s1;
         list[1] = s2;
-        list[2] = s3;
+        //list[2] = s3;
         for(int i=0; i<list.length; i++)
         {
             System.out.println(list[i].getName() + " " + list[i].getSid()); // s1,s2,s3
